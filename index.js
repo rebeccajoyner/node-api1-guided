bb3dde8f249469fbbd553f5e6153be814bcc673f
Rebecca Joyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-12-02 13:48:17 -0500

    hubs

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ const port = 4000;
 server.listen(port, () => 
     console.log(`\n ** API running on port ${port} ** \n`)
 );
+
+server.get("/hubs", (req, res) => {
+    db.find()
+    .then(hubs => {
+        res.status(200).json(hubs);
+    })
+    .catch(error => {
+        console.log("error on GET /hubs", error);
+        res
+        .status(500)
+        .json({ errorMessage: "error getting list of hubs of database"});
+    });
+});
