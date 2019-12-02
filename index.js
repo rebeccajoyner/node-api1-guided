de90a644ec4adb04ce6786efda51f727f2aa7b3b
Rebecca Joyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-12-02 13:09:41 -0500

    lec

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require("express"); 
+
+const server = express();
+
+server. get("/", (req, res) => {
+    res.send({ api: "up & running!"});
+});
+
+
+const port = 4000;
+server.listen(port, () => 
+    console.log(`\n ** API running on port ${port} ** \n`)
+);
